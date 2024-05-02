2c04faeac9d99ccea56be22407a527c9568d9925
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-05-02 12:08:32 -0300

    index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import freeRoutes from './free.routes';
 import authRoutes from './auth.routes';
 import contactRoutes from './contact.routes';
+import articleRouter from './articleRoutes';
 
 
 const router = Router()
@@ -9,5 +10,6 @@ const router = Router()
 router.use('/', freeRoutes)
 router.use('/', contactRoutes);
 router.use('/', authRoutes);
+router.use('/article', articleRouter)
 
 export default router
